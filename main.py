22ca2a8a2869d83de63ad87d4690a4483384a11b
Dipak Shah <dipakshah7555@gmail.com>
2026-07-07 07:30:06 +0545

    Fix indentation and yt-dlp options

--- main.py
+++ main.py
@@ -127,30 +127,26 @@ async def download_audio(url: str = Form(...), format: str = Form("mp3")):
     
     temp_id = str(uuid.uuid4())
     temp_path = os.path.join(UPLOAD_DIR, f"{temp_id}.%(ext)s")
-    
-ydl_opts = {
-    'format': 'bestaudio/best',
-    'outtmpl': temp_path,
-    'quiet': False,
-    'verbose': True,
-    'noplaylist': True,
-    'ffmpeg_location': '/usr/bin/ffmpeg',
 
-    'extractor_args': {
-        'youtube': {
-            'player_client': ['android']
-        }
-    },
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'outtmpl': temp_path,
+        'quiet': False,
+        'verbose': True,
+        'noplaylist': True,
+        'ffmpeg_location': '/usr/bin/ffmpeg',
+        'extractor_args': {
+            'youtube': {
+                'player_client': ['android']
+            }
+        },
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': format,
+            'preferredquality': '192',
+        }],
+    }
 
-    'postprocessors': [{
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': format,
-        'preferredquality': '192',
-    }],
-}
-    
-   
-    
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(url, download=True)
